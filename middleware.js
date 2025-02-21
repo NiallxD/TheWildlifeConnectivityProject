dc87023bd3e5128861f1cfb9fcdd914f87a5a947
NiallxD <niallbell@hotmail.co.uk>
2025-02-21 00:15:13 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,8 @@
 export default function middleware(req, res, next) {
     const password = process.env.PASSWORD; // Use the environment variable for the password
-    const authHeader = req.headers['authorization'];
-    
-    if (!authHeader || !authHeader.startsWith('Basic ')) {
+    if (req.headers['authorization'] !== `Basic ${Buffer.from(`admin:${password}`).toString('base64')}`) {
         res.status(401).send('Unauthorized');
         return;
     }
-
-    const base64Credentials = authHeader.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString().split(':');
-    
-    if (credentials[0] !== 'admin' || credentials[1] !== password) {
-        res.status(401).send('Unauthorized');
-        return;
-    }
-
     next();
 }
